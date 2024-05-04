33cf4793fb9119659bee95966858dd40a538d8fe
Jose Alvarez <alvarezmjose2021@gmail.com>
2024-05-04 08:24:52 -0300

    Ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Experiencia from "./Layout/Experiencia/Experiencia"
 import Footer from "./Layout/Footer/Footer"
 import Header from "./Layout/Header/Header"
 import Proyecto from "./Layout/Proyectos/Proyecto"
-import Servicios from "./Layout/Servicios/Servicios"
+// import Servicios from "./Layout/Servicios/Servicios"
 import SobreMi from "./Layout/SobreMi/SobreMi"
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <SobreMi />
       <Experiencia />
-      <Servicios />
+      {/* <Servicios /> */}
       <Proyecto />
       <Footer />
     </>
